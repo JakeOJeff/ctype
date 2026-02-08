9673ad15b18783f59b932416d242d15572d2ba3c
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-02-08 23:12:19 +0530

    Working on vars

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include <iostream> // for std::cout
 
-int main() {
-    std::cout << "Hello World";
+int main()
+{
+    int width;
+    width = 5;
+
+    std::cout << width;
+    width = 7;
+
+    std::cout << width
+    
+
     return 0;
 }
