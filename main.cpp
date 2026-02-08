3efe6d5991de7249ee420d9645b37db5ea82254f
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-02-09 05:20:46 +0530

    Vars arrays and vectors

--- main.cpp
+++ main.cpp
@@ -1,15 +1,43 @@
 #include <iostream> // for std::cout
-
+#include <vector>
+using namespace std;
 int main()
 {
-    int width;
-    width = 5;
+    // int age = 25;
+    // double price = 19.99;
+    // char grade = 'A';
+    // bool isStudent = true;
+    // std::string name = "Alice";
+    // char names[] = "Alice";
 
-    std::cout << width;
-    width = 7;
 
-    std::cout << width
+    // cout << "Enter your name " << names << " " << name <<endl <<"\n";
+    // cin >> name;
+    // cout  << "Enter your age"<<"\n";
+    // cin >> age;
+
+    // cout << "Hello " << name << ", you look amazing for " << age ;
+
+    // for (int i = 1; i <= 5; i++){
+    //     cout << i << " ";
+    // }
     
+    string fruits[] = {"apple", "banana", "orange"};
+
+    cout << fruits[2] << endl;
+
+    vector<int> numbers;
+    numbers.push_back(10);
+    numbers.push_back(20);
+
+    cout << numbers.size() << endl;
+    cout << numbers[0] << endl;
+
+    for (int num : numbers ) {
+        cout << num << " ";
+    }
+
+    numbers.pop_back();
 
     return 0;
 }
