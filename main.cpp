6de80ce282a64a7242c5193c633e8dd9af26a87f
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-02-09 14:12:37 +0530

    Overloading

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream> // for std::cout
+#include <cmath>
 #include <vector>
 #include <string>
+#include <math.h>
+
+
 using namespace std;
+
+int add(int a, int b);
+
+int add(int a, int b) {
+    return a + b;
+}
+
+double add(double a, double b){
+    return a + b;
+}
+
+double calculateDistance(int x1, int y1, int x2, int y2){
+    return sqrt( pow(y2 - y1,2) + pow(x2 - x1, 2) ) ;
+}
 int main()
 {
     // int age = 25;
@@ -40,28 +58,29 @@ int main()
 
     // numbers.pop_back();
 
-    string greeting = "Hello";
-    string name = "World";
+    // string greeting = "Hello";
+    // string name = "World";
 
-    string message = greeting + " " + name + "!";
+    // string message = greeting + " " + name + "!";
 
-    cout << message.length() << endl;
-    cout << message[0] << endl;
+    // cout << message.length() << endl;
+    // cout << message[0] << endl;
 
-    cout << message.substr(0, 5);
-    cout << message.find("World");
-    cout << message.replace(6, 5, "C++");
+    // cout << message.substr(0, 5);
+    // cout << message.find("World");
+    // cout << message.replace(6, 5, "C++");
 
-    string fullName;
+    // string fullName;
 
-    getline(cin, fullName);
+    // getline(cin, fullName);
 
-    if (fullName.length() > 0) {
-        cout << fullName;
-    }
+    // if (fullName.length() > 0) {
+    //     cout << fullName;
+    // }
     
-
-
+    cout << calculateDistance(0, 0, 20, 20);
+    cout << add(4, 3);
+    cout << add(3.2, 2.3);
 
     return 0;
 }
